4ff1bf931b8cc689b96604424f956f083c9cf905
Sukhad <sukhadadhikari3@gmail.com>
2024-01-31 00:15:51 -0500

    .d

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
         x++;
         if(x >= 5)
         {
-            document.querySelector('h1').innerHTML = 'There is not any (NO) option, try selecting!';
+            document.querySelector('h1').innerHTML = 'I am not going to select no';
         }
     }
    
