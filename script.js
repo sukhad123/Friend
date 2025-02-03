838d768132f26d2351f5076ccda92203c19d41a1
Sukhad Adhikari <101186753+sukhad123@users.noreply.github.com>
2025-02-03 01:11:10 +0000

    add

--- script.js
+++ script.js
@@ -11,22 +11,12 @@
     }
  })
 
- document.addEventListener("DOMContentLoaded",()=>
-    {
-       const imageElement = document.getElementById("image");
-       document.querySelector("#no").onclick= ()=>
-       {
-       imageElement.src = 'https://cdn-ak.f.st-hatena.com/images/fotolife/n/nativecamp_official/20220221/20220221122300.png'
-   
-       document.querySelector('h1').innerHTML = 'Please ma pani nepali ho';
-       document.querySelector('#button').innerHTML  = '';
-         
-       }
-    })
+
 
  document.addEventListener("DOMContentLoaded",()=>{
     let x = 0;
     document.querySelector("#no").onclick = ()=>{
+         imageElement.src = 'https://cdn-ak.f.st-hatena.com/images/fotolife/n/nativecamp_official/20220221/20220221122300.png'
         const randomLeft = Math.floor(Math.random() * (window.innerWidth-50));
         const randomTop = Math.floor(Math.random() * (window.innerHeight-50));
         document.querySelector("#no").style.position = "absolute";
